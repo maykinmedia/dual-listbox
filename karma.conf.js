cc75026831fed8258d8f796f4a20b9e060d76b5a
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-07-17 11:05:34 +0200

    Added a check if using CI

--- karma.conf.js
+++ karma.conf.js
@@ -17,11 +17,16 @@ webpackConfig.plugins = [];
 webpackConfig.externals = [];
 
 
+function ConfigException(message) {
+   this.message = message;
+   this.name = 'ConfigException';
+}
+
+
 // The main configuration
 module.exports = function(config) {
-    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-        console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
-        process.exit(1)
+    if (process.env.CI && (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY)) {
+        throw ConfigException('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
     }
 
     var customLaunchers = {
